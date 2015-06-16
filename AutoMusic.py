1a9bb1e1d6c5ccfe5078d3ec9aa0d1f29fcc25b7
jenfoxbot <fox.jennifer.r@gmail.com>
2015-06-16 02:56:44 -0700

    Create AutoMusic.py

---
+++ AutoMusic.py
@@ -0,0 +1,27 @@
+#Trigger music using IR sensor
+import os
+import time
+from subprocess import Popen
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN)
+
+file = '/home/pi/Desktop/example.mp3'
+
+try:
+        print "IR Breakbeam Test (CTRL+C to exit)"
+        time.sleep(2)
+        print "Ready!"
+
+        while True:        
+                if GPIO.input(23) == 0:
+                        print ('Motion Detected.')
+                        omxp = Popen(['omxplayer', file])
+                time.sleep(1)
+
+except KeyboardInterrupt:
+        print "\n Quit"
+
+GPIO.cleanup()
